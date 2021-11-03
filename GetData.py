b1d07185c622028966ea50be0ae4e6c79196cdb2
Meeko Racc <meekoracc@outlook.com>
2021-11-03 16:04:14 +0000

    Bring in a .env and start logging

--- GetData.py
+++ GetData.py
@@ -1,22 +1,30 @@
 import requests
 import pandas
 import asyncio
+import logging
+from dotenv import load_dotenv
+import os
 
 def main():
-#TODO Start detached task + logger
+  #Start logger
+  logging.basicConfig('%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='retreival.log')
+  logging.info('Starting Retrieval Task')
+  #Import API key and user token
+  load_dotenv()
+  #TODO Start detached task
 
-#TODO Import API key and user token
+  #TODO Request 320 oldest posts + time offset
 
-#TODO Request 320 oldest posts + time offset
+  #TODO Insert into database (MongoDB?)
 
-#TODO Insert into database (MongoDB?)
+  #TODO Requests capped at 2/sec so sleep for >= 0.5s
 
-#TODO Requests capped at 2/sec so sleep for >= 0.5s
+  #TODO iterate if MAXPOSTS variable reached or retrieved # is less than 320
 
-#TODO iterate if MAXPOSTS variable reached or retrieved # is less than 320
+  #TODO handle request code errors w/ recovery
 
-#TODO handle request code errors w/ recovery
-  pass
+  logging.info('Retrieval Task Stopped')
+  logging.shutdown()
 
 if __name__ == "__main__":
   main()
