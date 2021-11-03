5a9b2a02c41e242471207d7ce8b68374ac20a5ca
Meeko Racc <meekoracc@outlook.com>
2021-11-03 15:15:48 +0000

    Add what needs to be done for the data retrieval

---
+++ GetData.py
@@ -0,0 +1,22 @@
+import requests
+import pandas
+import asyncio
+
+def main():
+#TODO Start detached task + logger
+
+#TODO Import API key and user token
+
+#TODO Request 320 oldest posts + time offset
+
+#TODO Insert into database (MongoDB?)
+
+#TODO Requests capped at 2/sec so sleep for >= 0.5s
+
+#TODO iterate if MAXPOSTS variable reached or retrieved # is less than 320
+
+#TODO handle request code errors w/ recovery
+  pass
+
+if __name__ == "__main__":
+  main()
